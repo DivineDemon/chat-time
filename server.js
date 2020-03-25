f3665f330b5bcfdc0778c3bf06813a146f3d31f5
DivineDemon <supame123@gmail.com>
2020-03-25 23:22:28 +0400

    :sparkles: User and Room Mgmt. :construction: Add MongoDB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -11,14 +12,30 @@ const botName = 'ModeratorBot'
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
-io.on('connection', (socket) => {
-    socket.emit('message', formatMessage(botName, 'Welcome to Chat-Time!'))
-    socket.broadcast.emit('message', formatMessage(botName, 'A User has Joined the Chat'))
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A User has Left the Chat'))
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room)
+        socket.join(user.room)
+        socket.emit('message', formatMessage(botName, 'Welcome to Chat-Time!'))
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has Joined the Chat`))
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
     })
     socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('User', msg))
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    })
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has Left the Chat`))
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
     })
 })
 server.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
